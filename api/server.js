3c80837495b4625564556575f4cdfbec5de03f8c
Yahia-153 <yahiametwally153@gmail.com>
2025-01-25 17:50:08 +0200

    api update

--- api/server.js
+++ api/server.js
@@ -35,6 +35,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 module.exports = app;
 module.exports = (req, res) => {
-  res.json({ message: 'Server is running!' });
+  res.status(200).json({ message: 'Server is working!' });
 };
 

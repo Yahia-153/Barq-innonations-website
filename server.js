15afcdda1ed265f8e7be5c1b2865805c22976d4e
Yahia-153 <yahiametwally153@gmail.com>
2025-01-25 04:08:03 +0200

    Add CORS middleware to server for handling cross-origin requests

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+app.options('*', cors());
 app.get('', (req, res) => {
   const directoryPath = path.join(__dirname, 'imgs/team');
   fs.readdir(directoryPath, (err, files) => {

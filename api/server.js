a452041c2f264564f7ff1a15b292cf294598cf76
Yahia-153 <yahiametwally153@gmail.com>
2025-01-25 17:41:23 +0200

    Update server response to return folder data instead of status message

--- api/server.js
+++ api/server.js
@@ -35,6 +35,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 module.exports = app;
 module.exports = (req, res) => {
-  res.json({ message: 'Server is running!' });
+  res.json(folders);
 };
 
